26a52ded0225d4cc4eca185fd702ad0c25e8cbfd
Louis Merlin <louis.merlin@epfl.ch>
2018-09-15 13:59:00 +0200

    Change project navbar name

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ export default () => (
   <nav className="navbar" aria-label="main navigation">
     <div className="navbar-brand">
       <h1 className="title is-1 has-text-primary">
-        Name TBD
+        <span aria-label="asclepius" role="img">⚕️</span> asclepius
       </h1>
     </div>
   </nav>
