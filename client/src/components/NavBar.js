e982922ef0a301e49ce16db1a3b581639f7efc5d
Louis Merlin <louis.merlin@epfl.ch>
2018-09-15 11:26:02 +0200

    Add axa colors

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './NavBar.css';
+
+export default () => (
+  <nav className="navbar" aria-label="main navigation">
+    <div className="navbar-brand">
+      <h1 className="title is-1 has-text-primary">
+        Name TBD
+      </h1>
+    </div>
+  </nav>
+)
