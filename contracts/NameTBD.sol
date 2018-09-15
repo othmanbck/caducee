37f75b76eb62dbaf7f6ad92132b1e58f34278815
Louis Merlin <louis.merlin@epfl.ch>
2018-09-15 08:30:55 +0200

    Add doctor & pharmacy concepts

--- contracts/NameTBD.sol
+++ contracts/NameTBD.sol
@@ -2,17 +2,38 @@ pragma solidity ^0.4.24;
 
 contract NameTBD {
   address owner;
-  mapping(address => bool) isDoctor;
-  mapping(address => bool) isPharmacy;
+  mapping(address => bool) doctors;
+  mapping(address => bool) pharmacies;
   uint storedData;
 
   modifier isOwner() { require(msg.sender == owner); _; }
+  modifier isDoctor() { require(doctors[msg.sender]); _; }
+  modifier isPharmacy() { require(pharmacies[msg.sender]); _; }
 
   constructor() public {
     owner = msg.sender;
+    // For test purposes:
+    addDoctor(0xf17f52151EbEF6C7334FAD080c5704D77216b732);
+    addPharmacy(0xC5fdf4076b8F3A5357c5E395ab970B5B54098Fef);
   }
 
-  function set(uint x) public isOwner() {
+  function addDoctor(address doctor) public isOwner() {
+    doctors[doctor] = true;
+  }
+
+  function removeDoctor(address doctor) public isOwner() {
+    doctors[doctor] = false;
+  }
+
+  function addPharmacy(address pharmacy) public isOwner() {
+    pharmacies[pharmacy] = true;
+  }
+
+  function removePharmacy(address pharmacy) public isOwner() {
+    pharmacies[pharmacy] = false;
+  }
+
+  function set(uint x) public isDoctor() {
     storedData = x;
   }
 
