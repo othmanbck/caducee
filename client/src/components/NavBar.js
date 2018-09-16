469ebc00d98131c08d36403913d4c3bbf0cf04cf
otto-ben <othman.benchekroun@epfl.ch>
2018-09-16 08:48:54 +0200

    Add logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,13 +3,13 @@ import React, { Fragment } from 'react';
 export default props => (
   <nav className="navbar" aria-label="main navigation">
     <div className="navbar-brand">
-      <h1 className="title is-1 has-text-primary">
-        <span aria-label="caducée" role="img">⚕️</span> caducée
+      <h1 className="subtitle is-2 has-text-link">
+        <img src="/logo_light.png" style={{height: "40px"}} alt="caducée" />caducée
       </h1>
     </div>
     <div className="navbar-end">
       <div className="navbar-item">
-        <h2 className="subtitle is-2">
+        <h2 className="subtitle is-4 has-text-link">
           {props.statusLoaded && (
             props.isDoctor
             ? (<Fragment>Hello, Doctor <span aria-label="doctor" role="img">👩‍⚕️</span></Fragment>)
